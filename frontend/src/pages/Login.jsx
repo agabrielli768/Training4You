01f2c69b5c6598ba3edcdce218bcd926703e06a9
anthony_768 <agabrielli768@gmail.com>
2024-04-02 14:22:29 +0200

    commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 //redirige l'utilisateur vers sa page d'accueil
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 //composant de connexion
